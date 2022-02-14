49d5d3659642a87a01269c8b0b29804a4623ea25
warlegend24 <96477955+warlegend24@users.noreply.github.com>
2022-02-15 00:24:06 +0530

    Add files via upload

---
+++ assignment 3 output.py
@@ -0,0 +1,70 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 1
+ENTER A STRING:HELLO
+ENTER THE WORD/CHARACTER:L
+THE NO. OF OCCURENCES OF  L  IN  HELLO  =  2
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 2
+ENTER  DAY28
+ENTER MONTH2
+ENTER YEAR2016
+NEXT DATE = 29/2/ 2016
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 3
+[(3, 9), (9, 81), (10, 100)]
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 4
+ENTER GRADE POINT :-9
+Your Grade is 'A' and Excellent performance
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+======= RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ======
+QUESTION 6
+ENTER SID:1
+ENTER NAME:a
+DO YOU WANT TO ENTER MORE DATA? 'Y'(YES) or 'N'(NO)Y
+ENTER SID:2
+ENTER NAME:b
+DO YOU WANT TO ENTER MORE DATA? 'Y'(YES) or 'N'(NO)Y
+ENTER SID:3
+ENTER NAME:c
+DO YOU WANT TO ENTER MORE DATA? 'Y'(YES) or 'N'(NO)N
+SID =  1 NAME =  a
+SID =  2 NAME =  b
+SID =  3 NAME =  c
+sorted dictionary names=  ['a', 'b', 'c']
+sorted dictionary sid=  [1, 2, 3]
+ENTER SID3
+SID =  3  NAME =  c
+
+================================================ RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ================================================
+QUESTION 7
+ENTER NO. OF TERMS 5
+Fibonacci sequence:
+0
+1
+1
+2
+3
+AVERAGE =  1.4
+
+================================================ RESTART: C:\Users\HP\Desktop\COMPUTER(PYTHON)\assignment 3(Q1).py ================================================
+QUESTION 8
+a part = {1, 3, 5, 6, 8}
+b part = {17, 3, 6, 8, 9, 13}
+d part = {6, 7, 8, 9, 10}
+e part = {10, 7}
